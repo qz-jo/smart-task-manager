6412423e5a7f30bbcc0a9231f8fce2bed1d66be2
qz-jo <saifnabel800@gmail.com>
2026-08-11 23:09:46 +0300

    Prepare server for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -7,8 +7,7 @@ const jwt = require("jsonwebtoken");
 const db = require("./database");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(cors());
